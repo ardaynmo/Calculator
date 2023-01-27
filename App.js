03a70e65eae8d7208dfb8bc2547d2145f19eb097
Aditya kumar <adityashinex@gmail.com>
2023-01-27 10:50:06 +0530

    change

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Stack_navigator} from './src/components/stack_navigator';
 
 export default class App extends React.Component {
+  //app.js
   render() {
     return <Stack_navigator />;
   }
